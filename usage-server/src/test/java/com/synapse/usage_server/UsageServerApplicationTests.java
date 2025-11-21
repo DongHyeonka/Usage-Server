c84f7aee04fc591e09e9f96b88fde29673ca871c
root <root@hyeonD>
2025-11-21 17:13:48 +0900

    init : 프로젝트 생성

---
+++ usage-server/src/test/java/com/synapse/usage_server/UsageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synapse.usage_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsageServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
